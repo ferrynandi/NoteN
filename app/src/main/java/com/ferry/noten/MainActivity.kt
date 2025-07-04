125afe38a7f115eb54eec4080ab77bbae306241c
dikahr08 <hrdika08@gmail.com>
2025-07-05 03:47:50 +0700

    Update MainActivity.kt

--- app/src/main/java/com/ferry/noten/MainActivity.kt
+++ app/src/main/java/com/ferry/noten/MainActivity.kt
@@ -1,3 +1,38 @@
+package com.ferry.noten // sesuaikan nama projek
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.*
+import java.text.SimpleDateFormat
+import java.util.*
+import com.ferry.noten.ui.theme.NoteNTheme // seuaikan dengan nama project dan lanjut ke ui.theme/theme.kt
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteNTheme { //sesuaikan dengan nama
+                NoteApp()
+            }
+        }
+    }
+}
+
 data class NoteItem(val text: String, val timestamp: String)
 
 @Composable
